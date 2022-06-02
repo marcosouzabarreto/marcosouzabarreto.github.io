bb64ff7f970f6cacefa06219d530dee05c943684
Marco Barreto <marcosouzabarreto@gmail.com>
2022-06-02 13:08:51 -0300

    Deploy test

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload Marco.
+          Edit <code>src/App.js</code> and save to reload Marco Barreto.
         </p>
         <a
           className="App-link"
